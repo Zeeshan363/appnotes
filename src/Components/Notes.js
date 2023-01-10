cb1e1152744c0f17cfe1c58bdd7ad6ddd9b999bc
zeeshan363 <zsarfraz363@gmail.com>
2023-01-10 16:39:15 +0500

    now all done

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -8,7 +8,7 @@ const Notes = (props) => {
   return (
     <>
       {/* <div className="sm:w-[80%] mx-auto lg:w-[85%]"> */}
-      <div className="h-[180px] w-[80%] mx-auto shadow-lg shadow-gray-400 border-2 my-10 relative sm:w-[45%] lg:w-[27%] xl:w-[20%]">
+      <div className="h-[180px] w-[80%] mx-auto shadow-lg shadow-gray-400 border-2 my-10 relative sm:w-[45%] lg:w-[27%] xl:w-[26%]">
         <h1 className="px-2 my-3 mx-3">{props.title}</h1>
         <p className="px-2 my-3 mx-3 h-[110px]">{props.content}</p>
         <button onClick={deleteNote}>
